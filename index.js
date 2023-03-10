0bbce6592ad3a02d50fe05bda7fa917e1eec4789
mazidhussain <mazidhussain71@gmail.com>
2023-03-10 09:24:52 +0530

    Changes in code and pushed

--- index.js
+++ index.js
@@ -200,4 +200,4 @@ app.get('/bookings',async(req,res)=>{
     const userData = await getUserDataFromReq(req);
     res.json(await Booking.find({user:userData.id }).populate('place'));
 });
-app.listen(4000);
+app.listen(4000 || process.env.PORT);
